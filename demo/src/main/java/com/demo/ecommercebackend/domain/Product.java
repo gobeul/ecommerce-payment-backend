f552e54d5d2ac59dfef9bb90771422a8086e098e
gobeul <gotommao@gmail.com>
2025-11-18 23:02:12 +0900

    Entity (데이터모델)

---
+++ demo/src/main/java/com/demo/ecommercebackend/domain/Product.java
@@ -0,0 +1,28 @@
+package com.demo.ecommercebackend.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
